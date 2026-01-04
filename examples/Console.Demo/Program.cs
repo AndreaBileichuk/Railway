fbfb16a84e7f21914d6ca17e20bc9b40f27740d9
Andrea Bileichuk <bilejcukandrij@gmail.com>
2026-01-05 01:16:18 +0200

    Refactor the console example project

---
+++ examples/Console.Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Railway.Functional;
+
+Console.WriteLine("=== SUCCESS FLOW ===");
+
+Result<int>.Ok(100)
+    .OnSuccess(v => Console.WriteLine($"Initial value: {v}"))
+    .OnSuccess(v => Console.WriteLine($"Processed value: {v}"))
+    .OnFail(err => Console.WriteLine($"Error: {err}"));
+
+Console.WriteLine();
+Console.WriteLine("=== FAILURE FLOW ===");
+
+Result<int>.Fail("Something went wrong")
+    .OnSuccess(v => Console.WriteLine($"Value: {v}"))
+    .OnFail(err => Console.WriteLine($"Error: {err}"));
+
+Console.ReadLine();
