1f7521d39b99e1878d5efff7588ef8068631f1dd
Andrea Bileichuk <bilejcukandrij@gmail.com>
2026-01-04 13:19:57 +0200

    Implement Result and Result<T> classes. Add tests to make sure it's correct and consistent.

--- src/Railway.Functional/Result.cs
+++ src/Railway.Functional/Result.cs
@@ -2,5 +2,34 @@
 
 public class Result
 {
+    public bool IsSuccess { get; }
+    public string Error { get; }
+
+    protected Result(bool isSuccess, string error)
+    {
+        if (isSuccess && !string.IsNullOrWhiteSpace(error))
+            throw new InvalidOperationException();
+        
+        if(!isSuccess && string.IsNullOrWhiteSpace(error)) 
+            throw new InvalidOperationException();
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Ok() => new (true, string.Empty);
+    public static Result Fail(string message) => new (false, message);
+}
+
+public sealed class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(T? value, bool isSuccess, string error) : base(isSuccess, error)
+    {
+        Value = value;
+    }
     
+    public static Result<T> Ok(T value) => new (value, true, string.Empty);
+    public new static Result<T> Fail(string message) => new (default, false, message);
 }
---
+++ tests/Railway.Tests/ResultTests.cs
@@ -0,0 +1,46 @@
+﻿using Railway.Functional;
+
+namespace Railway.Tests;
+
+public class ResultTests
+{
+    [Fact]
+    public void Result_Ok_Should_Be_Success()
+    {
+        // Arrange & Act
+        var result = Result.Ok();
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Empty(result.Error);
+    }
+
+    [Fact]
+    public void Result_Fail_Should_Have_Error()
+    {
+        // Arrange
+        var message = "Something went wrong";
+        
+        // Act
+        var result = Result.Fail(message);
+        
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.Equal(message, result.Error);
+    }
+
+    [Fact]
+    public void ResultT_Ok_Should_Store_Value()
+    {
+        // Arrange
+        var value = 200;
+        
+        // Act
+        var result = Result<int>.Ok(value);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(value, result.Value);
+        Assert.Empty(result.Error);
+    }
+}
